1c9c468b727eb8ce7cc3a49d1068104e96db6a3d
Sidharth Jacob <sidharthpjacob@hotmail.com>
2015-09-04 23:22:13 +0530

    feat(login): Implement login page

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+// TODO Add Captcha later
+import '../styles/login.css';
+
 class Login {
   render() {
     return (
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div className="login-page">
+        <div className="login-title">Login</div>
+        <div className="username-padding">
+          <input className="username" type="text" placeholder="Username"/>
+        </div>
+        <div>
+          <input className="password" type="password" placeholder="Password"/>
+        </div>
+        <div className="forgot">Forgot Your Password ?</div>
+        <div className="submit-button">Submit</div>
       </div>
     );
   }
